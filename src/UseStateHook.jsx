e90ad09d39daa8ebfc06abedf6c0d33086b89269
Muqadas Urooj <muqadasurooj123@gmail.com>
2023-01-06 20:37:51 +0500

    Worked on useState Hook

---
+++ src/UseStateHook.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function UseStateHook() {
+    const [count,setCount]=useState(0);
+    return (
+      <div>   
+       <h1>You clicked my {count} Times</h1>
+       <button onClick={()=>setCount(count+1)}>
+        Click me
+       </button>
+      </div>
+    );
+  }
+  export default UseStateHook;
